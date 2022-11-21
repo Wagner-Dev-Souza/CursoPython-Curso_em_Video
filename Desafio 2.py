96446b8b2aa8eda40179090eb41fb5599346ea21
Wagner Silva Souza <souzawagner.ti@gmail.com>
2022-11-21 17:50:01 -0300

    add curso de Python da plataforma youtube - Gustavo Guanabara

---
+++ Desafio 2.py
@@ -0,0 +1,7 @@
+print ('===== DESAFIO 02 =====')
+print ('preencha com a data do seu nascimento')
+dia = input ('Dia (2 digitos)=')
+mes = input ('Mês (texto)=')
+ano = input ('Ano (4 digitos)=')
+print ('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
+
---
+++ Desafio 3.py
@@ -0,0 +1,5 @@
+print ('===== DESAFIO 03 =====')
+
+num1 = input ('Primeiro número =')
+num2 = input ('Segundo número=')
+print ('A soma é', num1 + num2)
