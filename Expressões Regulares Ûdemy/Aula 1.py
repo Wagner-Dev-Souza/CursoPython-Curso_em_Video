de1bc536c785d7ed398f3be22194c9e85e080d71
Wagner Silva Souza <souzawagner.ti@gmail.com>
2023-01-07 15:43:17 -0300

    adicionado a pasta de expressões regulares da Ûdemy

---
+++ Expressões Regulares Ûdemy/Aula 1.py
@@ -0,0 +1,18 @@
+# Regulamentação:
+# http://docs.python.org/3/library/re.html
+# http://docs.python.org/3/howto/regex.html#regex-howto
+
+import re
+
+# findall / search / sub
+# compile
+
+string = 'Este é um teste de expressões teste regulares.'
+print(re.search(r'teste', string))
+print(re.findall(r'teste', string))
+print(re.sub(r'teste', 'ABCD', string, count=1))
+
+regexp = re.compile(r'teste')
+print(regexp.search(string))
+print(regexp.findall(string))
+print(regexp.sub('DEF', string))
---
+++ Expressões Regulares Ûdemy/Aula 2.py
@@ -0,0 +1,13 @@
+# Meta caracteres: . ^ $ * + ? { } [ ] \ | ( )
+import re
+
+texto = '''
+João trouxe     flores para sua amada namrada em 10 de janeiro 1970,
+Maria era o nome dela.
+
+Foi um ano excelente na vida de joão. Teve 5 filhos, todos adultos atualmente.
+maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de
+pão de queijo.
+Não canso de ouvir a Maria:
+"Joooooooooãoooooooooo, o café ta prontinho aqui. Veeemm"!
+'''
